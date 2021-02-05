5811ca0103788f4167920398619b9065f30358bf
an.pham <an.pham@s3corp.com.vn>
2021-02-05 12:00:36 +0700

    update boundary checking function

--- src/hooks/useInternalBarcodeRead.ts
+++ src/hooks/useInternalBarcodeRead.ts
@@ -74,7 +74,7 @@ const useInternalBarcodeReadAndroid = (
         height: number;
         origin: Point<string>[];
       };
-      const _pointBounds = _bounds.origin.map(point => ({
+      const _pointBounds = _bounds.origin.map((point) => ({
         x: Number(point.x) / PixelRatio.get(),
         y: Number(point.y) / PixelRatio.get(),
       }));
@@ -89,43 +89,10 @@ const useInternalBarcodeReadAndroid = (
         );
       };
 
-      /**
-       *  0 --------------- 2
-       *  |  PDF417         |
-       *  | /////////////// |
-       *  1 --------------- 3
-       */
-      if (event.type === RNCamera.Constants.BarCodeType.pdf417) {
-        const [topLeft, bottomLeft, topRight, bottomRight] = _pointBounds;
-        if (
-          _insideBox(topLeft) &&
-          _insideBox(bottomLeft) &&
-          _insideBox(topRight) &&
-          _insideBox(bottomRight)
-        ) {
-          processingReadBarcode(event.data);
-          return;
-        }
-      }
-
-      /**
-       *  2 ------ 3
-       *  |
-       *  | QR Code
-       *  |
-       *  1 ------ 0
-       */
-      if (event.type === RNCamera.Constants.BarCodeType.qr) {
-        const [bottomRight, bottomLeft, topLeft, topRight] = _pointBounds;
-        if (
-          _insideBox(bottomRight) &&
-          _insideBox(bottomLeft) &&
-          _insideBox(topLeft) &&
-          _insideBox(topRight)
-        ) {
-          processingReadBarcode(event.data);
-          return;
-        }
+      const isInside = _pointBounds.every((p) => _insideBox(p));
+      if (isInside) {
+        processingReadBarcode(event.data);
+        return;
       }
     },
     [
